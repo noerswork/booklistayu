d04eba671cbc2e40e8cfb46f5f08e10c3aa0a1fc
Noer Ayu <ayu.intan117@gmail.com>
2020-04-30 12:03:50 +0700

    coba perbaiki crash 1

--- server.js
+++ server.js
@@ -13,11 +13,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.get('/', function (req, res) {
+    res.redirect('/routes');
+});
 
 // memuat route yang akan digunakan
 const routes = require('./routes/routes.js')(app, fs);
 
 // menentukan port server
 const server = app.listen(3001, () => {
-    console.log('listening on port %s...', server.address().port)
+    console.log('listening on port %s ...', server.address())
 });
